72e0d2afc5c557c8ea82eafbe3ca09e7a9a77319
vargac <simon.varga123@gmail.com>
2017-06-07 13:48:54 +0200

    tests: Add tests for motors and compass
    * Very strange robot's behaviour - heading still to the same direction
    
    Signed-off-by: vargac <simon.varga123@gmail.com>

---
+++ PickyStella/tests/test_PID/movements.ino
@@ -0,0 +1,273 @@
+Motor motorA = Motor(MOTOR_A_DIR_PIN, MOTOR_A_PWM_PIN);
+Motor motorB = Motor(MOTOR_B_DIR_PIN, MOTOR_B_PWM_PIN);
+Motor motorC = Motor(MOTOR_C_DIR_PIN, MOTOR_C_PWM_PIN);
+Motor motorD = Motor(MOTOR_D_DIR_PIN, MOTOR_D_PWM_PIN);
+
+void brake(void)
+{
+    motorA.off();
+    motorB.off();
+    motorC.off();
+    motorD.off();
+}
+
+void halt(void)
+{
+    motorA.stop();
+    motorB.stop();
+    motorC.stop();
+    motorD.stop();
+}
+
+int32_t P_term_2 = 0;
+int32_t I_term_2 = 0;
+int32_t D_term_2 = 0;
+uint32_t PID_last_time = 0;
+bool PID_reset = true;
+
+int16_t PID(int16_t speeds[4])
+{
+    int32_t min_limit = -1000;
+    int32_t max_limit = 1000;
+    for (uint8_t i = 0; i < 4; i++) {
+        if (255 - speeds[i] < max_limit) {
+            max_limit = 255 - speeds[i];
+        }
+        if (-255 - speeds[i] > min_limit) {
+            min_limit = -255 - speeds[i];
+        }
+    }
+
+    int32_t error = (int32_t)compass_angle();
+    if (error >= 180) error -= 360;
+
+    if (PID_reset) {
+        int32_t output = error * kP / 100;
+        if (output > max_limit) {
+            output = max_limit;
+        } else if (output < min_limit) {
+            output = min_limit;
+        }
+        PID_reset = false;
+        PID_last_time = micros();
+
+        return output;
+    }
+
+    int32_t d_time = micros() - PID_last_time;
+
+    P_term_2 = error * kP; // [error]%
+    int32_t output = (P_term_2 + D_term_2) / 100;
+
+    // if P_term_2 + D_term_2 exceeds limits, don't use I_term_2
+    if (output > max_limit) {
+        output = max_limit;
+        I_term_2 = 0;
+    } else if (output < min_limit) {
+        output = min_limit;
+        I_term_2 = 0;
+    } else {
+        I_term_2 += error * d_time / 1000 * kI / 1000; // [error]% * mS
+        output += I_term_2 / 100;
+        // solve reset-windup
+        if (output > max_limit) {
+            I_term_2 -= (output - max_limit) * 100;
+            output = max_limit;
+        } else if (output < min_limit) {
+            I_term_2 += (min_limit - output) * 100;
+            output = min_limit;
+        }
+    }
+
+    PID_last_time = micros();
+
+    return output;
+}
+
+void restart_PID()
+{
+    P_term_2 = I_term_2 = D_term_2 = 0;
+    PID_last_time = micros();
+    PID_reset = true;
+}
+
+void move_raw(int16_t speeds[4])
+{
+    motorA.go(speeds[0]);
+    motorB.go(speeds[1]);
+    motorC.go(speeds[2]);
+    motorD.go(speeds[3]);
+}
+
+void move_PID(int16_t speeds[4])
+{
+    int16_t compensation = PID(speeds);
+    motorA.go(speeds[0] + compensation);
+    motorB.go(speeds[1] + compensation);
+    motorC.go(speeds[2] + compensation);
+    motorD.go(speeds[3] + compensation);
+}
+
+inline void move_up(uint8_t spd)
+{
+    int16_t speeds[] = { spd, -spd, -spd, spd };
+    move_PID(speeds);
+}
+
+inline void move_right(uint8_t spd)
+{
+    int16_t speeds[] = { spd, spd, -spd, -spd };
+    move_PID(speeds);
+}
+
+inline void move_back(uint8_t spd)
+{
+    int16_t speeds[] = { -spd, spd, spd, -spd };
+    move_PID(speeds);
+}
+
+inline void move_left(uint8_t spd)
+{
+    int16_t speeds[] = { -spd, -spd, spd, spd };
+    move_PID(speeds);
+}
+
+inline void move_up_right(uint8_t spd)
+{
+    int16_t speeds[] = { spd, 0, -spd, 0 };
+    move_PID(speeds);
+}
+
+inline void move_back_right(uint8_t spd)
+{
+    int16_t speeds[] = { 0, spd, 0, -spd };
+    move_PID(speeds);
+}
+
+inline void move_back_left(uint8_t spd)
+{
+    int16_t speeds[] = { -spd, 0, spd, 0 };
+    move_PID(speeds);
+}
+
+inline void move_up_left(uint8_t spd)
+{
+    int16_t speeds[] = { 0, -spd, 0, spd};
+    move_PID(speeds);
+}
+
+inline void rotate(int16_t spd)
+{
+    int16_t speeds[] = { spd, spd, spd, spd };
+    move_raw(speeds);
+}
+
+/* may be dangerous */
+inline void centralize()
+{
+    int16_t spds[] = {0};
+    move_PID(spds);
+}
+
+/* VIC FUNCTIONS */
+
+void vic_move_PID(void)
+{
+    int16_t spds[4];
+
+    if (vic_args("%d %d %d %d", &spds[0], &spds[1], &spds[2], &spds[3]) == 4) {
+        move_PID(spds);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_restart_PID(void)
+{
+    restart_PID();
+}
+
+void vic_mA(void) { int16_t s; vic_args("%d", &s); motorA.go(s); }
+void vic_mB(void) { int16_t s; vic_args("%d", &s); motorB.go(s); }
+void vic_mC(void) { int16_t s; vic_args("%d", &s); motorC.go(s); }
+void vic_mD(void) { int16_t s; vic_args("%d", &s); motorD.go(s); }
+
+void vic_move_up(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_up(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_right(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_right(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_back(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_back(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_left(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_left(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_up_right(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_up_right(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_back_right(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_back_right(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_back_left(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_back_left(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
+
+void vic_move_up_left(void)
+{
+    uint8_t spd;
+    if (vic_args("%hhu", &spd) == 1) {
+        move_up_left(spd);
+    } else {
+        vic_print_err(VIC_ERR_INVALID_ARGS);
+    }
+}
---
+++ PickyStella/tests/test_PID/test_PID.ino
@@ -0,0 +1,122 @@
+#include <vic.h>
+#include <RelativeHMC6352.h>
+#include <Motor.h>
+#include "pinout.h"
+
+/* not used, just because of errors: '...' was not declared */
+#define ROTATE_SPEED 1
+#define TO_NORTH_THRESH 1
+
+#define kP 300 // percent
+#define kI 300 // percent
+#define kD 0
+
+#define PLOT_MIN -260
+#define PLOT_MAX 260
+
+uint32_t last_time = 0;
+int32_t P_term = 0;
+int32_t I_term = 0;
+int32_t D_term = 0;
+
+int16_t spds[] = { 150, -150, -150, 150 };
+
+void setup()
+{
+    Serial.begin(115200);
+    Serial.print("min");
+    Serial.print(" ");
+    Serial.print("max");
+    Serial.print(" ");
+    Serial.print("error");
+    Serial.print(" ");
+    Serial.print("P");
+    Serial.print(" ");
+    Serial.print("I");
+    Serial.print(" ");
+    Serial.print("D");
+    Serial.print(" ");
+    Serial.println("output");
+
+    compass_set_north();
+
+    last_time = micros();
+}
+
+/*
+d_time should not exceed 5 seconds
+*/
+void loop()
+{
+    static uint32_t change_set_point = millis();
+    if (millis() - change_set_point > 3000) {
+        compass_set_north_val((compass_north() - 90) % 360);
+        change_set_point = millis();
+    }
+
+
+
+
+
+    int32_t min_limit = -1000;
+    int32_t max_limit = 1000;
+    for (uint8_t i = 0; i < 4; i++) {
+        if (255 - spds[i] < max_limit) {
+            max_limit = 255 - spds[i];
+        }
+        if (-255 - spds[i] > min_limit) {
+            min_limit = -255 - spds[i];
+        }       
+    }
+
+    int32_t error = (int32_t)compass_angle();
+    if (error >= 180) {
+        error -= 360;
+    }
+    int32_t d_time = micros() - last_time;
+
+    P_term = error * kP; // [error]%
+    int32_t compensation = (P_term + D_term) / 100;
+
+    if (compensation > max_limit) {
+        compensation = max_limit;
+        I_term = 0;
+    } else if (compensation < min_limit) {
+        compensation = min_limit;
+        I_term = 0;
+    } else {
+        I_term += error * d_time / 1000 * kI / 1000; // [error]% * mS
+        compensation += I_term / 100;
+
+        if (compensation > max_limit) {
+            I_term -= (compensation - max_limit) * 100;
+            compensation = max_limit;
+        } else if (compensation < min_limit) {
+            I_term += (min_limit - compensation) * 100;
+            compensation = min_limit;
+        }
+    }
+
+    last_time = micros();
+
+    int16_t output_spds[4];
+    output_spds[0] = spds[0] + compensation;
+    output_spds[1] = spds[1] + compensation;
+    output_spds[2] = spds[2] + compensation;
+    output_spds[3] = spds[3] + compensation;
+    move_raw(output_spds);
+
+    Serial.print(PLOT_MIN);
+    Serial.print(" ");
+    Serial.print(PLOT_MAX);
+    Serial.print(" ");
+    Serial.print(error);
+    Serial.print(" ");
+    Serial.print(P_term / 100);
+    Serial.print(" ");
+    Serial.print(I_term / 100);
+    Serial.print(" ");
+    Serial.print(D_term);
+    Serial.print(" ");
+    Serial.println(compensation);
+}
