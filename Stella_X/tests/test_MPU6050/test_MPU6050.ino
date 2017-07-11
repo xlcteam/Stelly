4fe4082d5514c2901d1af72f7eb1b710066c58ae
vargac <simon.varga123@gmail.com>
2017-07-11 12:24:54 +0200

    tests: Test using MPU6050 instead of HMC6352
    * Lego compass is too weighty and HMC damaged. MPU6050 has much more features,
      although we are not going to use them in near future.
    * Library from Jrowberg added as submodule
    
    Signed-off-by: vargac <simon.varga123@gmail.com>

---
+++ Stella_X/tests/test_MPU6050/test_MPU6050.ino
@@ -0,0 +1,79 @@
+#include <I2Cdev.h>
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <Wire.h>
+
+MPU6050 mpu;
+bool dmpReady = false;
+uint8_t mpuIntStatus;
+uint8_t devStatus;
+uint16_t packetSize;
+uint16_t fifoCount;
+uint8_t fifoBuffer[64];
+Quaternion q;
+VectorFloat gravity;
+float euler[3];
+float ypr[3];
+
+void setup()
+{
+    Wire.begin();
+
+    Serial.begin(115200);
+
+    mpu.initialize();
+
+    if (mpu.testConnection() == true) {
+        Serial.println("connection succesful");
+    }
+    else {
+        Serial.println("connection failed");
+    }
+
+    devStatus = mpu.dmpInitialize();
+
+    if (devStatus == 0) {
+        mpu.setXGyroOffset(15);
+        mpu.setYGyroOffset(-53);
+        mpu.setZGyroOffset(20);
+        mpu.setXAccelOffset(-2865);
+        mpu.setYAccelOffset(-1322);
+        mpu.setZAccelOffset(2091);
+
+        mpu.setDMPEnabled(true);
+        dmpReady = true;
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    }
+    else {
+        Serial.println("error ");
+        Serial.println(devStatus);
+    }
+}
+
+void loop()
+{
+    if (dmpReady == false) {
+        return;
+    }
+
+    uint32_t time_start = micros();
+    mpu.resetFIFO();
+    fifoCount = mpu.getFIFOCount();
+    do {
+        fifoCount = mpu.getFIFOCount();
+    } while (fifoCount < packetSize);
+
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    mpu.dmpGetQuaternion(&q, fifoBuffer);
+    mpu.dmpGetGravity(&gravity, &q);
+    mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    uint32_t time_end = micros();
+
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[2] * 180/M_PI);
+    Serial.print("\ttime\t");
+    Serial.println(time_end - time_start);
+}
