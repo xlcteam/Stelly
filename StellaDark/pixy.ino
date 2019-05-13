4a033bbc76421512995c8a0ae5afe781c1ed0009
vargac <simon.varga123@gmail.com>
2019-05-13 17:46:33 +0200

    update: Fix getting data from PixyViSy
    Signed-off-by: vargac <simon.varga123@gmail.com>

--- StellaDark/pixy.ino
+++ StellaDark/pixy.ino
@@ -1,28 +1,26 @@
 
 uint16_t goal_distance, ball_distance;
-int8_t goal_angle;
+int16_t goal_angle;
 char action;
 uint32_t time, loop_count = 0;
 uint16_t min_time = ~0, max_time = 0, average_time = 0;
 
-int8_t angle_goal() {
-  static int8_t no_goal = 0;
-  static int8_t last_goal_angle = 0;
+int16_t angle_goal() {
+  static uint8_t no_goal_cnt = 0;
+  static int16_t last_goal_angle = GOAL_ANGLE_NO_GOAL;
   pixyViSy.update();
-  goal_angle = pixyViSy.getBallAngle();
-  if (goal_angle == ~0) {
-    no_goal++;
-    if (no_goal > 15) {
-      goal_angle = 0;
-      return 255;
-    }
-    else {   
+
+  if (pixyViSy.getBallDist() == ~(uint16_t)0) {
+    if (no_goal_cnt > 15) {
+      goal_angle = GOAL_ANGLE_NO_GOAL;
+    } else {
       goal_angle = last_goal_angle;
+      no_goal_cnt++;
     }
-  }
-  else {
-    no_goal = 0;
+  } else {
+    goal_angle = pixyViSy.getBallAngle();
     last_goal_angle = goal_angle;
+    no_goal_cnt = 0;
   }
   return goal_angle;
 }
@@ -31,7 +29,7 @@ int robot_angle() {
   static int last_relevant_angle = 0;
   int goal_angle;
   goal_angle = angle_goal();
-  if ( goal_angle == 255) {
+  if ( goal_angle == GOAL_ANGLE_NO_GOAL) {
     return compass() - last_relevant_angle;
   }
   else {
